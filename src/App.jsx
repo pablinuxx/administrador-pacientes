199ecd0615c520c12a713d92aaeedfd4c54b470a
Pachin <p.sarasua@hotmail.com>
2023-07-06 16:55:36 -0300

    agregando Local Storage

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./App.css";
 import { Header } from "./components/Header";
 import { Formulario } from "./components/Formulario";
 import { ListadoPacientes } from "./components/ListadoPacientes";
 
 function App() {
-  const [pacientes, setPacientes] = useState([]);
+  const initialState = () =>
+    JSON.parse(localStorage.getItem("pacientes")) || [];
+  const [pacientes, setPacientes] = useState(initialState);
   const [paciente, setPaciente] = useState({});
+
+  useEffect(() => {
+    localStorage.setItem("pacientes", JSON.stringify(pacientes));
+  }, [pacientes]);
 
   const eliminarPaciente = (id) => {
     const pacientesActualizados = pacientes.filter(
